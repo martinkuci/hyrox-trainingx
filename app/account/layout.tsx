28192b8a0b6efea916c6e3337930d744a9dc8165
martinkuci <martin.kuci@gmail.com>
2026-07-21 23:37:01 +0200

    Rename account help section

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -5,7 +5,7 @@ import { StickyHeader } from "@/components/navigation/StickyHeader";
 export default function AccountLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <StickyHeader title="Účet" fallbackHref="/" />
+      <StickyHeader title="Účet a pomoc" fallbackHref="/" />
       <div className="route-layout-safe">{children}</div>
       <StickyBottomNavigation />
     </>
